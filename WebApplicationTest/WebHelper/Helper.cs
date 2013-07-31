db4ffb507e6cc26318eb8e596567b1d84a77768c
hp-renhuozhu <ren-huo.zhu@hp.com>
2013-07-31 16:10:23 +0800

    add a helper project

---
+++ WebApplicationTest/WebHelper/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebHelper
+{
+    public class Helper
+    {
+        public string Name { get; set; }
+
+        public int Value{get;set;}
+
+        public void Test() 
+        {
+        
+        }
+    }
+}
